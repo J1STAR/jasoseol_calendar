c803644fe1cfe9a1ef69556cefa9657bbc1ba1c6
HanByul Jang <j.1star.0726@gmail.com>
2020-01-15 21:55:40 +0900

    [Add] dev config
    add dev config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     devServer: {
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "http://j1star.ddns.net:8000",
                 changeOrigin: true
             },
         },
